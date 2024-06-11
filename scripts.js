cd246fe5e07b2581bba8578df0296ddaa314cc8f
oliviervanembden <68058647+oliviervanembden@users.noreply.github.com>
2024-06-11 22:12:37 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -97,8 +97,11 @@ function showHint() {
 }
 
 const questions = {
-    1: { question: "Vraag 1: Wat is 5 + 3?", answer: "8" },
-    2: { question: "Locatie vinden: 8x - 186.372 = y en 16 - √x + 225.666 = y", answer: "161", hint: "Het snijpunt is een cordinaat." },
+    1: { question: "Volgens een gevangen genomen Noord Koreaanse hacker is de code: b-e+r-g", answer: "8", hint: "Dr. A. Scii weet het antwoord." },
+    2: { question: "Vraag 2: Los op: 8x - 186.372 = y, 16 - √x + 225.666", answer: "161", hint: "Het snijpunt is een coordinaat." },
+    3: { question: "Vraag 3: ", answer: "-10100", hint: "" },
+    4: { question: "Vraag 4: ", answer: "3288", hint: "" },
+    5: { question: "Iendvraag de code van de kluis is 8 de locatie is 161 voor de trein was de code -10100 en voor het kraken van de computer is 3288 ", answer: "-10500 / (161 – 3288 / 8) = 42", hint: "" },
     // Voeg meer vragen toe zoals nodig
 };
 
